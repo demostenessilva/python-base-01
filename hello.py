629f2a6f4dbb31ba0c9be354ce2e83a8f71a4e07
Demostenes Silva <demostenessilva@hotmail.com>
2025-01-11 01:46:55 -0300

    comentários com 3 aspas duplas para saber o que o programa faz

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+"""Hello World Multi Linguas.
+
+Dependendo da Lingua configurada no
+ambiente o programa exibe a mensagem
+correspondente.
+"""
 # Este é um comentário importante #
-
-
 print("Hello, World!")
 print("Olá Mundo!")
 print('brasil' .upper()) 
 print(56 - 7)
-
