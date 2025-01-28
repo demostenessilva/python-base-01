0e464eb9758c0f005982b3d116fb0f2ff1bd30c8
Demostenes Silva <demostenessilva@hotmail.com>
2025-01-28 01:28:36 -0300

    Documentando o código

--- hello.py
+++ hello.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3
 """Hello World Multi Linguas.
 
-Dependendo da Lingua configurada no
-ambiente o programa exibe a mensagem
-correspondente.
+Dependendo da Lingua configurada no ambiente, o programa exibe a mensagem correspondente.
 
 Como usar:
 
-Tenha a variável LANG devidamente configurada ex:
+Tenha a variável LANG devidamente configurada, por exemplo:
 
    export LANG=pt_BR
 
-Ou informe através do CLI argument `--lang`
+Ou informe a linguagem através do argumento de linha de comando `--lang`.
 
-Ou o usuário terá que digitar. 
+Ou o usuário terá que digitar o idioma manualmente.
 
 Execução:
 
@@ -21,38 +19,43 @@ Execução:
     ou
     ./hello.py
 """
-__version__ = "0.1.3"
-__author__ = "Silva"
-__license__ = "Unlicense"
+__version__ = "0.1.3"  # Versão do programa
+__author__ = "Silva"   # Autor do programa
+__license__ = "Unlicense"  # Licença do programa
 
 import os
 import sys
 
-# print(f"{sys.argv=}")
+# Dicionário para armazenar os argumentos passados via linha de comando
 arguments = {"lang": None, "count": 1}
+
+# Processa os argumentos passados via linha de comando
 for arg in sys.argv[1:]:
-    # TODO: Tratar ValueError
+    # TODO: Tratar ValueError (caso o argumento não esteja no formato chave=valor)
     key, value = arg.split("=")
-    key = key.lstrip("-").strip()
-    value = value.strip()
+    key = key.lstrip("-").strip()  # Remove hífens e espaços em branco da chave
+    value = value.strip()  # Remove espaços em branco do valor
     if key not in arguments:
+        # Mensagem de erro para opções inválidas
         print(f"Opção invalida `{key}`")
-        sys.exit()
-    arguments[key] = value
+        sys.exit()  # Encerra o programa se uma opção inválida for detectada
+    arguments[key] = value  # Atualiza o valor no dicionário de argumentos
 
-
-# Padrão snake case
+# Determina o idioma atual com base nos argumentos ou variáveis de ambiente
 current_language = arguments["lang"]
 if current_language is None:
-    # TODO: Usar repetição
+    # TODO: Usar repetição para evitar código duplicado
     if "LANG" in os.environ:
+        # Obtém o idioma da variável de ambiente LANG
         current_language = os.getenv("LANG")
     else:
+        # Solicita ao usuário que insira o idioma
         current_language = input("Qual o seu idioma:")
 
-
+# Limita o idioma aos primeiros 5 caracteres (ex: "pt_BR" -> "pt_BR")
 current_language = current_language[:5]
 
+# Dicionário com as mensagens de "Hello World" em diferentes idiomas
 msg = {
     "en_US": "Hello, World!",
     "pt_BR": "Olá Mundo!",
@@ -60,10 +63,6 @@ msg = {
     "es_SP": "Hola Mundo!",
     "fr_FR": "Bonjour Monde",
 }
-# sets (Hash Table) - O(1) - constante
-# dict (Hash Table)
 
-# Ordem de complexidade o(n)
-
-
+# Exibe a mensagem correspondente ao idioma configurado, repetida conforme o valor de "count"
 print(msg[current_language] * int(arguments["count"]))
